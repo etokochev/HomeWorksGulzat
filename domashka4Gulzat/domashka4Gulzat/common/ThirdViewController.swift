086781fc83e4dcdd81b994287e5277d99ac6d563
Erzhan Tokochev <slaverrt@icloud.com>
2023-03-05 19:48:18 +0600

    HomeWork #4

---
+++ domashka4Gulzat/domashka4Gulzat/common/ThirdViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ThirdViewController.swift
+//  domashka4Gulzat
+//
+//  Created by Erzhan Tokochev on 3/3/23.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    public var name1: String?
+    public var name2: String?
+    public var Imagethree: String = ""
+    public var r: Int = 0
+    
+    @IBOutlet weak var imageViewThree: UIImageView!
+    @IBOutlet weak var labelNameThree: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
+    @IBAction func buttonBuyThree(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        navigationController?.popToRootViewController(animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageViewThree.layer.cornerRadius = 15
+        imageViewThree.layer.borderWidth = 2
+        imageViewThree.layer.borderColor = UIColor.gray.cgColor
+        imageViewThree.clipsToBounds = true
+        switch r {
+        case 0: imageViewThree.image = UIImage(named: "kreslo1")
+        case 1: imageViewThree.image = UIImage(named: "kreslo2")
+        case 2: imageViewThree.image = UIImage(named: "kreslo3")
+        default: print("Error")
+        }
+        print(r)
+        labelNameThree.text = name1
+        label2.text = name2
+    }
+}
